4e732f0ba9eb79231e64467c4c5822e2a56d66d8
Kavec1 <matus.kavec@gmail.com>
2019-02-20 17:30:28 +0100

    Update zavlazovac.ino

--- zavlazovac/zavlazovac.ino
+++ zavlazovac/zavlazovac.ino
@@ -3,11 +3,13 @@
 
 const int humSensor = A0;
 const int tempSensor = A1;
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-const int interruptPin =;
+const byte rs = 12, en = 11, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+const byte interruptPin = 2;
+const byte pumpPin = 3;
 
-volatile int humValue;
-volatile int tempValue;
+volatile float humValue;
+volatile float tempValue;
+volatile byte state = TRUE;
 
 float mapFloat(long x, float in_min, float in_max, float out_min, float out_max){
   return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
@@ -17,20 +19,34 @@ void setup() {
   LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
   lcd.begin(16, 2);
   lcd.print("Zavlazovac");
+
   pinMode(13, OUTPUT);
-  Timer1.initialize(2000000); // initialize timer1, and set a 1 second period
-  Timer1.attachInterrupt(Blink); // attaches Blink() as a timer interrupt function
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), change, FALLING);
+  Timer1.initialize(2000000); // initialize timer1, and set and period in microseconds
+  Timer1.attachInterrupt(sensorReading); // attaches sensorReading() as a timer interrupt function
 }
 
-void Blink(){
+void sensorReading(){
   digitalWrite(13, digitalRead(13) ^ 1);
   humValue = analogRead(humSensor);
-  tempValue = mapFloat(analogRead(tempSensor),0,1023,0,4.5); 
+  tempValue = mapFloat(analogRead(tempSensor),0,1023,0,445.11); 
+}
+
+void change(){
+  state = !state;
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  float voltage = mapfloat(sensorValue,0,1023,0,4.5);
+  if (state){
   lcd.setCursor(0, 1);
-  lcd.print(voltage);
-  //lcd.print(millis()/1000);
+  lcd.print(Soil moisture:);
+  lcd.setCursor(0, 2);
+  lcd.print(humValue);
+}else{
+  lcd.setCursor(0, 1);
+  lcd.print(Temperature:);
+  lcd.setCursor(0, 2);
+  lcd.print(tempValue);
+}
 }
