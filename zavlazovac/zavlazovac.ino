c09966e3c83cee250fced9b6e052a7ae27754d85
Kavec1 <matus.kavec@gmail.com>
2019-02-20 17:08:00 +0100

    Update zavlazovac.ino

--- zavlazovac/zavlazovac.ino
+++ zavlazovac/zavlazovac.ino
@@ -1,18 +1,22 @@
 #include <LiquidCrystal.h>
 #include "TimerOne.h"
 
+const int humSensor = A0;
+const int tempSensor = A1;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-volatile int sensorValue;
+const int interruptPin =;
 
-float mapfloat(long x, float in_min, float in_max, float out_min, float out_max){
+volatile int humValue;
+volatile int tempValue;
+
+float mapFloat(long x, float in_min, float in_max, float out_min, float out_max){
   return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
 }
 
 void setup() {
-  // put your setup code here, to run once:
+  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
   lcd.begin(16, 2);
-  lcd.print("zavlazovac");
+  lcd.print("Zavlazovac");
   pinMode(13, OUTPUT);
   Timer1.initialize(2000000); // initialize timer1, and set a 1 second period
   Timer1.attachInterrupt(Blink); // attaches Blink() as a timer interrupt function
@@ -20,7 +24,8 @@ void setup() {
 
 void Blink(){
   digitalWrite(13, digitalRead(13) ^ 1);
-  sensorValue = analogRead(A0);
+  humValue = analogRead(humSensor);
+  tempValue = mapFloat(analogRead(tempSensor),0,1023,0,4.5); 
 }
 void loop() {
   // put your main code here, to run repeatedly:
